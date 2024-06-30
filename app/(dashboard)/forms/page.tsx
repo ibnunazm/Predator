348daccc3a3663a9d560a6b3d9480e203d6ebf63
Ibnu Naz'm Ar-rosyid <ibnunazm.a11@gmail.com>
2024-06-30 20:57:08 +0700

    forms responsive

--- app/(dashboard)/forms/page.tsx
+++ app/(dashboard)/forms/page.tsx
@@ -7,16 +7,14 @@ export const metadata: Metadata = {
 const DataTablesPage = () => {
   return (
     <div>
-      <div className="bg-gray-100 p-8">
-        <div className="bg-white p-6 rounded-lg shadow-lg max-w-4xl mx-auto">
-          <h1 className="text-xl font-bold mb-4">
+      <div className="bg-gray-100 p-4 md:p-8">
+        <div className="bg-white p-4 md:p-6 rounded-lg shadow-lg max-w-4xl mx-auto">
+          <h1 className="text-lg md:text-xl font-bold mb-4">
             PT Torabika Eka Semesta - Divisi Instant Creamer
           </h1>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Plant:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Plant:</label>
               <input
                 type="number"
                 className="w-full p-2 border rounded"
@@ -24,9 +22,7 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                No. SPK:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">No. SPK:</label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -34,17 +30,13 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Date:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Date:</label>
               <input type="date" className="w-full p-2 border rounded" />
             </div>
           </div>
-          <div className="grid grid-cols-3 gap-4 mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Department:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Department:</label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -52,9 +44,7 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Shift:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Shift:</label>
               <input
                 type="number"
                 className="w-full p-2 border rounded"
@@ -62,9 +52,7 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Line:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Line:</label>
               <input
                 type="number"
                 className="w-full p-2 border rounded"
@@ -72,11 +60,9 @@ const DataTablesPage = () => {
               />
             </div>
           </div>
-          <div className="grid grid-cols-3 gap-4 mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Area:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Area:</label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -84,9 +70,7 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Machine:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Machine:</label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -94,18 +78,13 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Time:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Time:</label>
               <input type="time" className="w-full p-2 border rounded" />
             </div>
           </div>
-
-          <div className="grid grid-cols-3 gap-4 mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                System Condition:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">System Condition:</label>
               <div className="flex items-center">
                 <input
                   type="radio"
@@ -130,9 +109,7 @@ const DataTablesPage = () => {
               </div>
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Priority:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Priority:</label>
               <div className="flex items-center">
                 <input
                   type="radio"
@@ -167,16 +144,19 @@ const DataTablesPage = () => {
                 </label>
               </div>
               <div className="flex items-center">
-                <input type="radio" id="low" name="priority" className="mr-2" />
+                <input
+                  type="radio"
+                  id="low"
+                  name="priority"
+                  className="mr-2"
+                />
                 <label htmlFor="low" className="text-gray-700">
                   Low (7 Days)
                 </label>
               </div>
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Jenis Pekerjaan:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Jenis Pekerjaan:</label>
               <div className="flex items-center">
                 <input
                   type="radio"
@@ -212,61 +192,48 @@ const DataTablesPage = () => {
               </div>
             </div>
           </div>
-
-          <div className="grid grid-cols-2 gap-4 mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Problem:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Problem:</label>
               <textarea
                 className="w-full p-2 border rounded"
                 placeholder="Describe the problem"
               ></textarea>
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Repair Suggestion:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Repair Suggestion:</label>
               <textarea
                 className="w-full p-2 border rounded"
                 placeholder="Provide repair suggestion"
               ></textarea>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-4 mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Cause Analysis:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Cause Analysis:</label>
               <textarea
                 className="w-full p-2 border rounded"
                 placeholder="Analyze the cause"
               ></textarea>
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Repair Actions:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Repair Actions:</label>
               <textarea
                 className="w-full p-2 border rounded"
                 placeholder="Describe repair actions"
               ></textarea>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-4 mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Start Time:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Start Time:</label>
               <input
                 type="datetime-local"
                 className="w-full p-2 border rounded"
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                End Time:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">End Time:</label>
               <input
                 type="datetime-local"
                 className="w-full p-2 border rounded"
@@ -274,19 +241,15 @@ const DataTablesPage = () => {
             </div>
           </div>
           <div className="grid grid-cols-1 mt-4">
-            <label className="block text-gray-700 font-bold mb-2">
-              Result:
-            </label>
+            <label className="block text-gray-700 font-bold mb-2">Result:</label>
             <textarea
               className="w-full p-2 border rounded"
               placeholder="Describe the result"
             ></textarea>
           </div>
-          <div className="grid grid-cols-3 gap-4 mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Performed by:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Performed by:</label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -294,9 +257,7 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Verified by:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Verified by:</label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -304,9 +265,7 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">
-                Accepted by:
-              </label>
+              <label className="block text-gray-700 font-bold mb-2">Accepted by:</label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
