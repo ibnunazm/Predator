4cd614a64acea9bb921899c4b5c239ca1fbf20f9
Ibnu Naz'm Ar-rosyid <ibnunazm.a11@gmail.com>
2024-06-30 22:55:57 +0700

    forms

--- app/(dashboard)/forms/page.tsx
+++ app/(dashboard)/forms/page.tsx
@@ -1,7 +1,7 @@
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "DataTables | Horizon UI by Ories",
+  title: "Forms | Predator",
 };
 
 const DataTablesPage = () => {
@@ -14,7 +14,9 @@ const DataTablesPage = () => {
           </h1>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Plant:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Plant:
+              </label>
               <input
                 type="number"
                 className="w-full p-2 border rounded"
@@ -22,7 +24,9 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">No. SPK:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                No. SPK:
+              </label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -30,13 +34,17 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Date:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Date:
+              </label>
               <input type="date" className="w-full p-2 border rounded" />
             </div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Department:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Department:
+              </label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -44,7 +52,9 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Shift:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Shift:
+              </label>
               <input
                 type="number"
                 className="w-full p-2 border rounded"
@@ -52,7 +62,9 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Line:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Line:
+              </label>
               <input
                 type="number"
                 className="w-full p-2 border rounded"
@@ -62,7 +74,9 @@ const DataTablesPage = () => {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Area:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Area:
+              </label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -70,7 +84,9 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Machine:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Machine:
+              </label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -78,13 +94,17 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Time:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Time:
+              </label>
               <input type="time" className="w-full p-2 border rounded" />
             </div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">System Condition:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                System Condition:
+              </label>
               <div className="flex items-center">
                 <input
                   type="radio"
@@ -109,7 +129,9 @@ const DataTablesPage = () => {
               </div>
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Priority:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Priority:
+              </label>
               <div className="flex items-center">
                 <input
                   type="radio"
@@ -144,19 +166,16 @@ const DataTablesPage = () => {
                 </label>
               </div>
               <div className="flex items-center">
-                <input
-                  type="radio"
-                  id="low"
-                  name="priority"
-                  className="mr-2"
-                />
+                <input type="radio" id="low" name="priority" className="mr-2" />
                 <label htmlFor="low" className="text-gray-700">
                   Low (7 Days)
                 </label>
               </div>
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Jenis Pekerjaan:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Jenis Pekerjaan:
+              </label>
               <div className="flex items-center">
                 <input
                   type="radio"
@@ -194,14 +213,18 @@ const DataTablesPage = () => {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Problem:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Problem:
+              </label>
               <textarea
                 className="w-full p-2 border rounded"
                 placeholder="Describe the problem"
               ></textarea>
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Repair Suggestion:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Repair Suggestion:
+              </label>
               <textarea
                 className="w-full p-2 border rounded"
                 placeholder="Provide repair suggestion"
@@ -210,30 +233,78 @@ const DataTablesPage = () => {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Cause Analysis:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Cause Analysis:
+              </label>
               <textarea
                 className="w-full p-2 border rounded"
                 placeholder="Analyze the cause"
               ></textarea>
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Repair Actions:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Repair Actions:
+              </label>
               <textarea
                 className="w-full p-2 border rounded"
                 placeholder="Describe repair actions"
               ></textarea>
             </div>
           </div>
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-4 mt-4">
+            <div className="col-span-7">
+              <label className="block text-gray-700 font-bold mb-2">
+              Spareparts Usage
+              </label>
+              <textarea
+                className="w-full p-2 border rounded"
+                placeholder="Analyze the cause"
+              ></textarea>
+            </div>
+            <div className="col-span-5">
+              <label className="block text-gray-700 font-bold mb-2">
+                Executor
+              </label>
+              <div className="executor">
+                <label className="radio mr-1">
+                  <input
+                    type="radio"
+                    name="executor"
+                    className="m-1"
+                  />
+                  Internal
+                </label>
+                <label className="radio mr-1">
+                  <input
+                    type="radio"
+                    name="executor"
+                    className="m-1"
+                  />
+                  Eksternal
+                </label>
+              </div>
+              <input
+                type="text"
+                className="w-full p-2 border rounded"
+                placeholder="Input Executor"
+              />
+            </div>
+          </div>
+
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Start Time:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Start Time:
+              </label>
               <input
                 type="datetime-local"
                 className="w-full p-2 border rounded"
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">End Time:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                End Time:
+              </label>
               <input
                 type="datetime-local"
                 className="w-full p-2 border rounded"
@@ -241,7 +312,9 @@ const DataTablesPage = () => {
             </div>
           </div>
           <div className="grid grid-cols-1 mt-4">
-            <label className="block text-gray-700 font-bold mb-2">Result:</label>
+            <label className="block text-gray-700 font-bold mb-2">
+              Result:
+            </label>
             <textarea
               className="w-full p-2 border rounded"
               placeholder="Describe the result"
@@ -249,7 +322,9 @@ const DataTablesPage = () => {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Performed by:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Performed by:
+              </label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -257,7 +332,9 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Verified by:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Verified by:
+              </label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
@@ -265,7 +342,9 @@ const DataTablesPage = () => {
               />
             </div>
             <div className="col-span-1">
-              <label className="block text-gray-700 font-bold mb-2">Accepted by:</label>
+              <label className="block text-gray-700 font-bold mb-2">
+                Accepted by:
+              </label>
               <input
                 type="text"
                 className="w-full p-2 border rounded"
